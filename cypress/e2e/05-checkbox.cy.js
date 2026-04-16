c86b8ce697864bc460d11e2a7081a4a1d08aa1e1
ro7an22 <87031341+rohan-ailani@users.noreply.github.com>
2026-04-16 19:28:38 -0400

    adding a comment

--- cypress/e2e/05-checkbox.cy.js
+++ cypress/e2e/05-checkbox.cy.js
@@ -4,6 +4,8 @@ describe('Checkbox Page', () => {
 
   it('should interact with checkbox tree', () => {
 
+// this test will fail due to the ad overlay blocking the checkbox tree to appear. please look at defect 5 in DEFECTS.md
+
     checkboxPage.visit();
 
     checkboxPage.expandAll();
